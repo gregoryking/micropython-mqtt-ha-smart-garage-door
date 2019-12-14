2368921aca055a8834bf9d12347e30e1fd6d45d8
Greg King <greg@kooji..com>
2019-12-14 23:19:45 +0000

    Updated pin scheme for latest hardware setup

--- config.py
+++ config.py
@@ -9,10 +9,12 @@ AVAILABILITY_TOPIC = "home/garage/garagedoor/available"
 
 
 # Hardware Pin scheme
-#       v+12V                       vRX
-# [12] [x] [x]            [4][0][X][1][3]
-#           ^GND                 ^VDD  ^TX
+# corrected?
+#         Y                             Y     ?                      Y
+# [ + ] [12 ] [ - ] [ 5 ] [ x ] [ x ] [ 4 ] [ 0 ] [  ] [ + ] [ 3 ] [ 1 ] 
+#                                                         VDD   RX   TX
+
 RELAY_PIN=12
 OPEN_SENSOR_PIN=4
 CLOSED_SENSOR_PIN=1
-PUSH_BUTTON_PIN=3
+PUSH_BUTTON_PIN=5

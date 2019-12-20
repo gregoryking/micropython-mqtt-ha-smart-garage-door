14c1086773a7323968955125e428223a374a5f6d
Greg King <greg@kooji..com>
2019-12-20 23:14:35 +0000

    Wide ranging confug updates

--- config.py
+++ config.py
@@ -1,20 +1,23 @@
+# Wireless Credentials
 ESSID = ***REMOVED***
 PASSWORD = ***REMOVED***
-SERVER = "192.168.1.128"
 
+# MQTT
+SERVER = "192.168.1.128"
 COMMAND_TOPIC = "home/garage/garagedoor/set"
 STATE_TOPIC = "home/garage/garagedoor"
 TARGET_TOPIC = "home/garage/garagedoortarget"
 AVAILABILITY_TOPIC = "home/garage/garagedoor/available"
 
+# Full time taken for door to move from open to closed or vice versa (ms)
+TRANSITION_DURATION = 15000
 
 # Hardware Pin scheme
-# corrected?
-#         Y                             Y     ?                      Y
-# [ + ] [12 ] [ - ] [ 5 ] [ x ] [ x ] [ 4 ] [ 0 ] [  ] [ + ] [ 3 ] [ 1 ] 
+# +12v  GPIO  GND   GPIO              GPIO  GPIO  +3.3v +3.3v GPIO  GPIO
+# [ + ] [12 ] [ - ] [ 5 ] [ x ] [ x ] [ 4 ] [ 0 ] [ + ] [ + ] [ 3 ] [ 1 ] 
 #                                                         VDD   RX   TX
 
 RELAY_PIN=12
 OPEN_SENSOR_PIN=4
-CLOSED_SENSOR_PIN=1
-PUSH_BUTTON_PIN=5
+CLOSED_SENSOR_PIN=3  # Would ideally free up this sensor pin to allow serial debugging and updates
+PUSH_BUTTON_PIN=0

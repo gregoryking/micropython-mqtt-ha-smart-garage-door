880315c0c5c146ee0336fb6e739ce4f6df661e1f
Greg King <greg@kooji..com>
2020-08-20 22:30:56 +0100

    mockmotor speed-up unit tests run rates

--- kooji/motor/mockmotor_test.py
+++ kooji/motor/mockmotor_test.py
@@ -3,12 +3,8 @@ from kooji.motor.mockmotor import MockMotor
 from kooji.enums import Movement, Position
 import uasyncio as asyncio
 
-# print("file is:{}".format(__file__))
-# print("name is:{}".format(__name__))
 
-unit_test_transition_duration = 100
-unit_test_refresh_ms = 10
-
+UNIT_TEST_SPEEDUP_FACTOR = 100
 
 loop = asyncio.get_event_loop()
 
@@ -20,7 +16,8 @@ async def execute_and_wait(coro, wait):
         coro()
     else:
         coro()
-        await asyncio.sleep(wait)
+        wait = int(wait / UNIT_TEST_SPEEDUP_FACTOR)
+        await asyncio.sleep_ms(wait)
 
 
 async def execute_and_wait_seq(seq, coro):
@@ -33,7 +30,7 @@ def setup_and_run(seq):
     def door_status_cb(dummy1, dummy2):
         pass
 
-    mm = MockMotor(door_status_cb=door_status_cb, loop=loop, log_detailed_progress=True)
+    mm = MockMotor(door_status_cb=door_status_cb, loop=loop, log_detailed_progress=True, time_to_open=int(MockMotor.TRANSITION_TIME_MS  / UNIT_TEST_SPEEDUP_FACTOR), debounce_ms=int(MockMotor.DEBOUNCE_MS  / UNIT_TEST_SPEEDUP_FACTOR), current_time_to_open=int(MockMotor.TRANSITION_TIME_MS / UNIT_TEST_SPEEDUP_FACTOR))
 
     coro = mm.toggle
     # loop.run_until_complete(simple_open_stop_close(loop))
@@ -56,7 +53,7 @@ class DoorActuatorRunRequests(unittest.TestCase):
         self.assertTrue(position == Position.OPEN)
 
     def test_triple_click_double_click_stop_close(self):
-        position = setup_and_run([0, 0, 2, 0, 2, None])
+        position = setup_and_run([0, 0, 2000, 0, 2000, None])
         self.assertTrue(position == Position.CLOSED)
 
     def test_go_nuts(self):
@@ -64,15 +61,15 @@ class DoorActuatorRunRequests(unittest.TestCase):
         self.assertTrue(position == Position.OPEN)
 
     def test_double_click_pause_click(self):
-        position = setup_and_run([0, 2, None])
+        position = setup_and_run([0, 2000, None])
         self.assertTrue(position == Position.PART_OPEN)
 
     def test_double_click_stop(self):
-        position = setup_and_run([0, 2, None])
+        position = setup_and_run([0, 2000, None])
         self.assertTrue(position == Position.PART_OPEN)
 
     def test_open_stop_close(self):
-        position = setup_and_run([2, 2, None])
+        position = setup_and_run([2000, 2000, None])
         self.assertTrue(position == Position.CLOSED)
 
     def test_open(self):
@@ -92,7 +89,7 @@ class DoorActuatorRunRequests(unittest.TestCase):
         self.assertTrue(position == Position.OPEN)
 
     def test_open_stop_close_stop_open(self):
-        position = setup_and_run([4, 2, 2, 3, None])
+        position = setup_and_run([4000, 2000, 2000, 3000, None])
         self.assertTrue(position == Position.OPEN)
 
 

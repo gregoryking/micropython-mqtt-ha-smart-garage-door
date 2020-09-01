8b16b3e9265142773f2fb6f6aa1777425d6e577b
Greg King <greg@kooji..com>
2020-09-01 14:27:13 +0100

    Removing missing / redundant config import

--- kooji/motor/motor.py
+++ kooji/motor/motor.py
@@ -1,7 +1,7 @@
 import uasyncio as asyncio
 from kooji.primitives.switch import Switch
 from kooji.machine import Pin, Signal
-from config import RELAY_PIN, MOVE_PULSE_DURATION, STOP_PULSE_DURATION
+from config import RELAY_PIN, MOVE_PULSE_DURATION
 
 class Motor:
     def __init__(self):

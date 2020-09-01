8290c0ff1c40c076f7de4831fb38f2b6bd9adcce
Greg King <greg@kooji..com>
2020-09-01 15:32:57 +0100

    Corrected motor to use signals and not switches

--- kooji/motor/motor.py
+++ kooji/motor/motor.py
@@ -6,14 +6,13 @@ from config import RELAY_PIN, MOVE_PULSE_DURATION
 class Motor:
     def __init__(self):
         relay_pin = Pin(RELAY_PIN[0], Pin.OUT, RELAY_PIN[1])
-        relay_signal = Signal(relay_pin, invert=RELAY_PIN[1])
-        self.__relay_switch = Switch(relay_signal)
+        self.__relay_signal = Signal(relay_pin, invert=RELAY_PIN[1])
         self.__loop = asyncio.get_event_loop()
 
     def toggle(self):
         self.__loop.create_task(self.__pulse_coro(MOVE_PULSE_DURATION))
 
     async def __pulse_coro(self, period):
-        self.__relay_switch.on()
+        self.__relay_signal.on()
         asyncio.sleep_ms(period)
-        self.__relay_switch.off()
+        self.__relay_signal.off()
